30e848d517b808e7c80b8a2bb53ba8ac8286fede
NahomiTorres04 <naho_1998-2@outlook.es>
2020-10-29 17:30:30 -0600

    Plantilla responsive lista

---
+++ pwa_parcial/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+<div id="app">
+    <header>
+      <Logo></Logo>
+      <Navmovil></Navmovil>
+    <Nav></Nav>
+    </header>
+    <main>
+      <Banner></Banner>
+      <Descripcion></Descripcion>
+      <Cards></Cards>
+      <Cards></Cards>
+      <Registro></Registro>
+      <Fotos></Fotos>
+      <Album></Album>
+      <Footer></Footer>
+    </main>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav'
+import Navmovil from './components/Navmovil'
+import Logo from './components/Logo'
+import Banner from './components/Banner'
+import Descripcion from './components/Descripcion'
+import Cards from './components/Cards'
+import Registro from './components/Registro'
+import Fotos from './components/Fotos'
+import Album from './components/Album'
+import Footer from './components/Footer'
+
+export default {
+  name: 'app',
+  components: {
+    Nav,
+    Navmovil,
+    Logo,
+    Banner,
+    Descripcion,
+    Cards,
+    Registro,
+    Fotos,
+    Album,
+    Footer
+  }
+}
+</script>
+
+<style>
+:root {
+    --bckg: white;
+    --color-secundario: #912c44;
+    --color-nav: #FFE5EB;
+    --rgba-hover-nav: rgba(255,255,255,.6);
+    --color-submenu: rgba(255,229,235,0.7) ;
+    --color-fuente-nav: black;
+    --color-fuente-logo-titulo: black;
+    --color-fuente-logo-contenido: black;
+    --color-fuente-hover-nav: #912c44;
+    --gradientbanner: rgba(255, 255, 255, 0.6);
+    --bckgreg: #E5EAF0;
+    --bckglog: #ffffff;
+    --color-button-reg: #ff668a;
+    --color-border-reg: #949494;
+
+    /**Fuentes/ */
+    --font-titulo: 'Satisfy', cursive;
+    --font-subtitulo: 'Special Elite', cursive;
+    --font-content: 'Titillium Web', sans-serif;
+}
+body {
+  margin: 0;
+}
+.text-center {
+  text-align: center;
+}
+
+
+
+</style>
---
+++ pwa_parcial/src/components/Logo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="Logo" class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="item">
+            <h1>Hotel Confortable</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Logo"
+}
+</script>
+
+<style scoped>
+    #Logo {
+        background-color: var(--bckglog);
+        height: 150px;
+        }
+    #Logo .container .row{
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+    }
+    .container .row .item {
+        padding: 25px 0;
+      }
+    .container .row .item h1 {
+          font-family: var(--font-subtitulo);
+          margin: 0;
+          padding: 5px;
+          font-size: 45px;
+          color: var(--color-fuente-logo-titulo);
+          font-weight: bolder;
+        }
+    .container .row .item p {
+          font-family: var(--font-content);
+          font-size: 15px;
+          color: var(--color-fuente-logo-contenido);
+          margin-bottom: 0;
+        }
+</style>
